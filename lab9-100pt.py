29bebfea30f387a71a23e3f802890c9ce258bc19
MiguelBalderrama <miguel.balderrama.alexander@gmail.com>
2014-10-15 16:04:07 -0500

    lab9 100pt

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,17 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+while x == 1:
+    print "what is your temperature?"
+    userinput1 = int(raw_input())
+    print "have you been sick in the last 24 hours?"
+    userinput2 = raw_input()
+    print "have you recently travelled to West Africa?"
+    userinput3 = raw_input()
+    if userinput1 >= 105 and userinput2 == "no":
+        print "ok a doctor will visit you soon"
+    elif userinput1 >= 102 and userinput2 == "yes":
+        print "please wait for further diagnostics"
+    elif userinput3 == "yes" and userinput1 >= 100:
+        print "sounds like ebola dont touch anyone"
+    print                                                                                                                                                                  
